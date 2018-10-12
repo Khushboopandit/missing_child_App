88822e44ce025d8683c243dfb3494e502bd0504c
Khushboopandit <khushboo17@navgurukul.org>
2018-10-12 20:56:00 +0530

    pushing to github

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SignUp from './components/signUp.js'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SignUp/>
+      </div>
+    );
+  }
+}
+
+export default App;
