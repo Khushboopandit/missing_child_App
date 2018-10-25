41bb7565fe9e7753a414b41e7d76eff81369726a
Khushboopandit <khushboo17@navgurukul.org>
2018-10-25 20:58:16 +0530

    Updates

---
+++ precache-manifest.28ae1359a1c3a18c015fec00432c48e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f6a99c3e35b20979aca4ba630b4c5be",
+    "url": "/missing_child_App/static/media/bg-01.8f6a99c3.jpg"
+  },
+  {
+    "revision": "549af8518e0feb941a1a",
+    "url": "/missing_child_App/static/js/runtime~main.549af851.js"
+  },
+  {
+    "revision": "da75df1dff49084b8c99",
+    "url": "/missing_child_App/static/js/main.da75df1d.chunk.js"
+  },
+  {
+    "revision": "0a25b17399448c7678b7",
+    "url": "/missing_child_App/static/js/1.0a25b173.chunk.js"
+  },
+  {
+    "revision": "da75df1dff49084b8c99",
+    "url": "/missing_child_App/static/css/main.f14e3712.chunk.css"
+  },
+  {
+    "revision": "0a25b17399448c7678b7",
+    "url": "/missing_child_App/static/css/1.196ab960.chunk.css"
+  },
+  {
+    "revision": "f93da58f00c24e71cc0f2c3b5f3da7bf",
+    "url": "/missing_child_App/index.html"
+  }
+];
